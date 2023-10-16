30946d2ddd9d9b1490d7b35dd20b04caa5a6f0e6
muratacarr <acarrmuratt@gmail.com>
2023-10-16 22:58:47 +0300

    Add project files.

---
+++ FluentValidation/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidationApp.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FluentValidationApp.Context
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ FluentValidation/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentValidationApp.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public int Age { get; set; }
+    }
+}
