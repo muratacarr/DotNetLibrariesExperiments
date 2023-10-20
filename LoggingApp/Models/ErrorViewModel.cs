27154a5c59c81bee4d822da420530e509191bd8f
muratacarr <acarrmuratt@gmail.com>
2023-10-20 09:55:12 +0300

    Logging başlangıç

---
+++ LoggingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoggingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
