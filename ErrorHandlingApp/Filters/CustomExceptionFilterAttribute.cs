9c7abc0a6c6f407a1cdcffb80914ffdc53909cbd
muratacarr <acarrmuratt@gmail.com>
2023-10-23 12:02:32 +0300

    Error Handling tamamlandı

---
+++ ErrorHandlingApp/Filters/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace ErrorHandlingApp.Filters
+{
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public string ErrorPage { get; set; } = "Error";
+        public override void OnException(ExceptionContext context)
+        {
+            var result=new ViewResult() { ViewName=ErrorPage };
+            result.ViewData=new ViewDataDictionary(new EmptyModelMetadataProvider(),context.ModelState);
+
+            result.ViewData.Add("Exception",context.Exception);
+            result.ViewData.Add("Url", context.HttpContext.Request.Path.Value);
+
+            context.Result=result;
+
+        }
+    }
+}
---
+++ ErrorHandlingApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews(options =>
+{
+    //options.Filters.Add(new CustomExceptionFilterAttribute() { ErrorPage = "Error1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStatusCodePages(async statusCodeContext =>
+{
+    // using static System.Net.Mime.MediaTypeNames;
+    statusCodeContext.HttpContext.Response.ContentType = Text.Plain;
+
+    await statusCodeContext.HttpContext.Response.WriteAsync(
+        $"Bir Hata var. Durum kodu : {statusCodeContext.HttpContext.Response.StatusCode}");
+});
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
