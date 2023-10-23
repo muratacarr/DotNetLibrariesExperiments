008f8a75cce5c479f0ba2be02c390aa59b1f841f
muratacarr <acarrmuratt@gmail.com>
2023-10-23 13:00:50 +0300

    Environment tamamlandı

---
+++ EnvironmentApp/Program.cs
@@ -0,0 +1,31 @@
+using static System.Net.Mime.MediaTypeNames;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStatusCodePages(async statusCodeContext =>
+{
+    statusCodeContext.HttpContext.Response.ContentType = Text.Plain;
+
+    await statusCodeContext.HttpContext.Response.WriteAsync($"{app.Environment.EnvironmentName}");
+});
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
